8c73428026d768bf1bf94a72e2a95921d11fb4fc
ZhuckovAlex <91392232+ZhuckovAlex@users.noreply.github.com>
2023-11-07 19:30:49 +0300

    website_zakaz

--- script.js
+++ script.js
@@ -8,108 +8,6 @@ const iconMenu=document.querySelector('.header__menu-icon');
 	});
 	}
 /*---------------------------------*/
-const architecturalBureau = document.querySelector('.architectural-bureau');
-const services = document.querySelector('.services');
-const projects = document.querySelector('.projects');
-const advantages = document.querySelector('.advantages');
-
-const scrollItems = document.querySelectorAll('section,footer');
-
-const scrollAnimation = () => {
-    let windowCenter = /*(window.innerHeight / 6) +*/ window.scrollY + 200;
-  
-    scrollItems.forEach(el => {
-    	
-    	let scrolloffset = el.offsetTop;/*+ (el.offsetHeight /6)*/
-    	
-    	if(windowCenter >= scrolloffset){
-    		el.classList.add('animation-class');
-    	} else {
-    		el.classList.remove('animation-class');
-    	}
-    });
-
-}
-scrollAnimation();
-window.addEventListener('scroll',() => {
-	scrollAnimation();
-});
-
-/*---------анимация иконок и заголовков-------*/
-
-const servicesItem = document.querySelectorAll('.services__item');
-
-
-let iconAnimation = () => {
-let windowCenter = window.scrollY + 100;	
-
-servicesItem.forEach(el => {
-	    let servicesImg = el.querySelector('.services__svg');
-	    let servicesTitle = el.querySelector('.services__item-title');
-		let scrolloffset = el.offsetTop;
-   	  
-    	if(windowCenter >= scrolloffset){
-            servicesTitle.classList.add('animation-class');
-    		servicesImg.classList.add('animation-class');
-    	} else {
-    		servicesTitle.classList.remove('animation-class');
-    		servicesImg.classList.remove('animation-class');
-    	}
-});
-}
-iconAnimation();
-window.addEventListener('scroll',() => {
-	iconAnimation();
-});
-/*-------анимация квадрата-------------*/
-const cube = document.querySelectorAll('.rect');
-const time = 4000;
-const step = 100;
-
-function outNum(num, elem){
-	
-	let l = document.querySelector('.' + elem);
-	let n = 0; 
-	let t = Math.round(time/(num/step));
-	let interval = setInterval(() => {
-		n = n + step;
-		if (n == num){
-			clearInterval(interval);
-		}
-		l.innerHTML = n;
-	},
-t);
-}
-let cubeAnimation = () => {
-let windowCenter = window.scrollY + 300;
-let scrolloffset = advantages.offsetTop;
-
-        if(windowCenter >= scrolloffset ){
-        	outNum(1000, 'number1');
-             outNum(500, 'number2');
-             outNum(400, 'number3'); 
-           cube.forEach(el => {
-          
-                el.classList.add('animation-class');
-                
-           }); 
-
-        } else {
-    		 cube.forEach(el => {
-              el.classList.remove('animation-class');
-           	
-           });
-        
-}
-}
-
-cubeAnimation();
-window.addEventListener('scroll',() => {
-	cubeAnimation();
-
-});
-
-/*-------------------------------------*/
 
 const modal = document.querySelector('.consultation__popup');
 const consultationPopupBtn = modal.querySelector('.consultation__popup-btn');
@@ -151,4 +49,92 @@ e.preventDefault();
 popupButton.addEventListener('click', (e) => {
   
     closeModal();
+});
+/*-----------------------------------*/
+
+const animItems = document.querySelectorAll('._anim-items');
+console.log(animItems);
+
+if(animItems.length > 0) {
+	
+	window.addEventListener('scroll', animOnScroll);
+	function animOnScroll() {
+		for(let index = 0; index < animItems.length; index++) {
+			const animItem = animItems[index];
+			const animItemHeight = animItem.offsetHeight;
+			const animItemOffset = offset(animItem).top;
+			const animStart = 4;
+
+			let animItemPoint = window.innerHeight - animItemHeight / animStart;
+			if (animItemHeight > window.innerHeight){
+               animItemPoint = window.innerHeight - window.innerHeight / animStart;
+			}
+			
+			if((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+				animItem.classList.add('_active');
+			}
+            
+
+		}
+	}
+	function offset(el) {
+		const rect = el.getBoundingClientRect(),
+		scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+		scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+		return { top: rect.top + scrollTop, left: rect.left + scrollLeft}
+	}
+	
+
+	setTimeout(() => {
+		animOnScroll();
+	}, 300);
+	
+}
+/*---------цифры-----------*/
+const advantages = document.querySelector('.advantages');
+
+const time = 4000;
+const step = 100;
+
+
+function outNum(num, elem){
+	
+	let l = document.querySelector('.' + elem);
+	let n = 0; 
+	let t = Math.round(time/(num/step));
+	let interval = setInterval(() => {
+		n = n + step;
+		if (n == num){
+			clearInterval(interval);
+		}
+		l.innerHTML = n;
+	},
+t);
+}
+/*-----------анимация квадрата---------*/
+const cube = document.querySelectorAll('.rect');
+let cubeAnimation = () => {
+let windowCenter = window.scrollY + 300;
+let scrolloffset = advantages.offsetTop;
+
+        if(advantages.classList.contains("_active") && advantages.classList.contains("_start") ){
+        	outNum(1000, 'number1');
+             outNum(500, 'number2');
+             outNum(400, 'number3'); 
+             advantages.classList.remove('_start');
+           cube.forEach(el => {
+          
+                el.classList.add('animation-class');
+                
+           }); 
+
+        } 
+        
+}
+
+
+cubeAnimation();
+window.addEventListener('scroll',() => {
+	cubeAnimation();
+
 });
--- script2.js
+++ script2.js
@@ -1,3 +1,8 @@
+/*---------gallery--------------*/
+$(function() {
+	$(".fancybox").fancybox();
+});
+/*-----------burger-------------*/
 const iconMenu=document.querySelector('.header__menu-icon');
 	const menuBody=document.querySelector('.header__menu');
 	if(iconMenu){      
@@ -7,3 +12,46 @@ const iconMenu=document.querySelector('.header__menu-icon');
 		menuBody.classList.toggle('_active');
 	});
 	}
+/*----------------анимация-------------------*/
+const animItems = document.querySelectorAll('._anim-items');
+console.log(animItems);
+
+if(animItems.length > 0) {
+	
+	window.addEventListener('scroll', animOnScroll);
+	function animOnScroll() {
+		for(let index = 0; index < animItems.length; index++) {
+			const animItem = animItems[index];
+			const animItemHeight = animItem.offsetHeight;
+			const animItemOffset = offset(animItem).top;
+			const animStart = 4;
+
+			let animItemPoint = window.innerHeight - animItemHeight / animStart;
+			if (animItemHeight > window.innerHeight){
+               animItemPoint = window.innerHeight - window.innerHeight / animStart;
+			}
+			
+			if((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+				animItem.classList.add('_active');
+			}
+            
+
+		}
+	}
+	function offset(el) {
+		const rect = el.getBoundingClientRect(),
+		scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+		scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+		return { top: rect.top + scrollTop, left: rect.left + scrollLeft}
+	}
+	
+
+	setTimeout(() => {
+		animOnScroll();
+	}, 300);
+	
+}
+
+
+/*--------------------------------------*/
+	
